d8931c9db952ab6acd23774278749502bff2baf7
ayase <paper.airplane01@gmail.com>
2020-06-26 20:56:50 +0900

    Use swift-format.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,24 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      VStack(alignment: .leading) {
-        Text("Turtle Rock")
-          .font(.title)
-        HStack {
-          Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
-            .font(.subheadline)
-          Spacer()
-          Text(/*@START_MENU_TOKEN@*/"California"/*@END_MENU_TOKEN@*/)
-            .font(.subheadline)
-        }
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text("Turtle Rock")
+        .font(.title)
+      HStack {
+        Text( /*@START_MENU_TOKEN@*/"Joshua Tree National Park" /*@END_MENU_TOKEN@*/)
+          .font(.subheadline)
+        Spacer()
+        Text( /*@START_MENU_TOKEN@*/"California" /*@END_MENU_TOKEN@*/)
+          .font(.subheadline)
       }
-    .padding()
     }
+    .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
