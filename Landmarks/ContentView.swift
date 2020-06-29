e02da01cc6ca5f08cadce787c563553ba3d1cc69
ayase <paper.airplane01@gmail.com>
2020-06-29 13:23:03 +0900

    Compose the detail view

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,19 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    VStack(alignment: .leading) {
-      Text("Turtle Rock")
-        .font(.title)
-      HStack {
-        Text( /*@START_MENU_TOKEN@*/"Joshua Tree National Park" /*@END_MENU_TOKEN@*/)
-          .font(.subheadline)
+    VStack {
+      MapView()
+        .edgesIgnoringSafeArea(.top)
+        .frame(height: 300)
+      
+      CircleImage()
+        .offset(y: -130)
+        .padding(.bottom, -130)
+      
+      VStack(alignment: .leading) {
+          Text("Turtle Rock")
+            .font(.title)
+            .multilineTextAlignment(.leading)
+          HStack {
+            Text( /*@START_MENU_TOKEN@*/"Joshua Tree National Park" /*@END_MENU_TOKEN@*/)
+              .font(.subheadline)
+            Spacer()
+            Text( /*@START_MENU_TOKEN@*/"California" /*@END_MENU_TOKEN@*/)
+              .font(.subheadline)
+          }
+        }
+        .padding()
+      
         Spacer()
-        Text( /*@START_MENU_TOKEN@*/"California" /*@END_MENU_TOKEN@*/)
-          .font(.subheadline)
       }
     }
-    .padding()
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
