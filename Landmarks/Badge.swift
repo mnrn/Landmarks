ff1d3b173659ace060e568b60ac426205339a22d
ayase <paper.airplane01@gmail.com>
2020-07-02 14:03:56 +0900

    Combine the Badge Foreground and Background

--- Landmarks/Badge.swift
+++ Landmarks/Badge.swift
@@ -9,13 +9,32 @@
 import SwiftUI
 
 struct Badge: View {
-    var body: some View {
-        BadgeBackground()
+  static let rotationCount = 8
+  
+  var badgeSymbols: some View {
+    ForEach(0..<Badge.rotationCount) {i in
+      RotatedBadgeSymbol(
+        angle: .degrees(Double(i) / Double(Badge.rotationCount)) * 360.0
+      )
     }
+    .opacity(0.5)
+  }
+  
+  var body: some View {
+    ZStack {
+      BadgeBackground()
+      GeometryReader { geometry in
+        self.badgeSymbols
+          .scaleEffect(1.0 / 4.0, anchor: .top)
+          .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+      }
+    }
+  .scaledToFit()
+  }
 }
 
 struct Badge_Previews: PreviewProvider {
-    static var previews: some View {
-        Badge()
-    }
+  static var previews: some View {
+    Badge()
+  }
 }
---
+++ Landmarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by mnrn on 02/07/2020.
+//  Copyright © 2020 mnrn. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+  let angle: Angle
+  var body: some View {
+    BadgeSymbol()
+    .padding(-60)
+      .rotationEffect(angle, anchor: .bottom)
+  }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+  static var previews: some View {
+    RotatedBadgeSymbol(angle: .init(degrees: 5))
+  }
+}
