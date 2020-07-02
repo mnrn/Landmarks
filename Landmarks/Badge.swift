fb86d022ee9dff0b7895d9c6e1d1a29620dae6e0
ayase <paper.airplane01@gmail.com>
2020-07-02 13:30:56 +0900

    Draw the badge Background

---
+++ Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by mnrn on 02/07/2020.
+//  Copyright © 2020 mnrn. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        BadgeBackground()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
---
+++ Landmarks/BadgeSymbol.swift
@@ -0,0 +1,21 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by mnrn on 02/07/2020.
+//  Copyright © 2020 mnrn. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"BadgeSymbol"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+  static var previews: some View {
+    BadgeSymbol()
+  }
+}
