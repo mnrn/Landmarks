8298c045a5ad4bd0bb106cd2e6d7ba9b57d167a3
ayase <paper.airplane01@gmail.com>
2020-07-06 12:47:46 +0900

    Compose the Home View

--- Landmarks/CategoryRow.swift
+++ Landmarks/CategoryRow.swift
@@ -15,19 +15,35 @@ struct CategoryRow: View {
   var body: some View {
     VStack(alignment: .leading) {
       Text(self.categoryName)
-          .font(.headline)
-          .padding(.leading, 15)
-          .padding(.top, 5)
+        .font(.headline)
+        .padding(.leading, 15)
+        .padding(.top, 5)
       
       ScrollView(.horizontal, showsIndicators: false) {
         HStack(alignment: .top, spacing: 0) {
           ForEach(self.items) { landmark in
-            Text(landmark.name)
+            CategoryItem(landmark: landmark)
           }
         }
       }
       .frame(height: 185)
     }
+  }
+}
+
+
+struct CategoryItem: View {
+  var landmark: Landmark
+  var body: some View {
+    VStack(alignment: .leading) {
+      landmark.image
+        .resizable()
+        .frame(width: 155, height: 155)
+        .cornerRadius(5)
+      Text(landmark.name)
+        .font(.caption)
+    }
+    .padding(.leading, 15)
   }
 }
 
