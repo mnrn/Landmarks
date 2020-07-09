d5c039d4d2c3c39b39cab20831eb05c92e637668
ayase <paper.airplane01@gmail.com>
2020-07-09 21:28:52 +0900

    Add a watchOS Target

---
+++ WatchLandmarks Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Landmarks
+//
+//  Created by mnrn on 09/07/2020.
+//  Copyright © 2020 mnrn. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WatchLandmarks Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  Landmarks
+//
+//  Created by mnrn on 09/07/2020.
+//  Copyright © 2020 mnrn. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
